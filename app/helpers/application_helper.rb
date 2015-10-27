942a39afabc0c48ec65cb21a53f43b882da94b91
Jason Curran <jc@jasoncurran.com>
2015-10-27 19:59:17 -0400

    setup css and dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def body_class(get_class)
+    content_for(:body_class) { get_class }
+  end
+
+  def title(get_title)
+    content_for(:title) { get_title }
+  end
+
 end
