13b98da589a456cf2eaee3d12ab53feeaef1e1f5
Jason Curran <jc@jasoncurran.com>
2015-10-27 16:48:22 -0400

    updated seed data for following/follower

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,11 @@ twizzles = Twizzle.create([
   { user_id: 3, content: 'Twizzle 2 by Britney Spears' }, 
   { user_id: 3, content: 'Twizzle 3 by Britney Spears' }
 ])
+
+relationships = Relationship.create([
+  { follower_id: 2, followed_id: 1 }, 
+  { follower_id: 3, followed_id: 1 }, 
+  { follower_id: 1, followed_id: 2 }, 
+  { follower_id: 1, followed_id: 3 },
+  { follower_id: 2, followed_id: 3 }
+])
