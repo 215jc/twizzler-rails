9d7f068e221423e0328bd37321f9298ae6f6e5a5
Jason Curran <jc@jasoncurran.com>
2015-10-27 21:14:15 -0400

    added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
     poststylus = require('poststylus');
+    watch = require('gulp-watch');
 
-gulp.task('stylus', function () {
-  gulp.src('app/assets/stylesheets/app.styl')
-    .pipe(stylus({
-      use: [
-        poststylus(['rucksack-css', 'lost'])
-      ]
-    }))
-    .pipe(gulp.dest('public/css'))
+gulp.task('watch', function () {
+   gulp.watch('app/assets/**/*.styl', ['stylus']);
 });
 
-gulp.task('default', ['stylus']);
+gulp.task('stylus', function () {
+    gulp.src('app/assets/stylesheets/app.styl')
+      .pipe(stylus({
+        use: [
+          poststylus(['rucksack-css', 'lost'])
+        ]
+      }))
+      .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('default', ['stylus', 'watch']);
